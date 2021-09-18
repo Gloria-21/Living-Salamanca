0a4758ae9c7e38140dea854f6934ba5f42110764
Gloria-21 <gloriahn_82@hotmail.com>
2021-09-18 20:41:37 +0000

    add funtion send email as it was deleted accidentally

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendEmail(contactForm) {
+    emailjs.send("service_7wjurxm", "Spanish courses", {
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.emailaddress.value,
+        "information_request": contactForm.informationsummary.value,
+    })
+     .then(
+        function(response) {
+          console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+        return false;
+}
